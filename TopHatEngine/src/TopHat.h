b008ec914bb64ea4aeb7010546a33a908c846259
Hatman2s <thomas.sheppard2411@gmail.com>
2023-10-22 12:40:35 +0300

    stole (joking ofc) glfw keycodes to be used client side

--- TopHatEngine/src/TopHat.h
+++ TopHatEngine/src/TopHat.h
@@ -8,7 +8,15 @@
 #include "TopHat/Application.h"
 #include "TopHat/Layer.h"
 #include "TopHat/Log.h"
+
 #include "TopHat/Events/Events.h"
+#include "TopHat/Events/KeyEvents.h"
+#include "TopHat/Events/MouseEvents.h"
+#include "TopHat/Events/ApplicationEvents.h"
+
+#include "TopHat/Input.h"
+#include "TopHat/TopHatKeys.h"
+#include "TopHat/TopHatMouseButtons.h"
 //-----------------------------------
 #include "TopHat/Main.h"
 //-----------------------------------
